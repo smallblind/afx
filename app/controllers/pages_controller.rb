38d061b600ac81d84025b3880a2634f5a4eeb994
Keith Pattison <keith.pattison.tamu@gmail.com>
2014-05-01 17:56:28 -0500

    Intial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,26 @@
+class PagesController < ApplicationController
+    def home
+    end
+
+    def about
+    end
+
+    def contact
+    end
+
+    def oath
+    end
+
+    def signals
+    end
+
+    def rules
+    end
+
+    def members
+    end
+
+    def history
+    end
+    
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afx::Application.config.secret_token = '25ce7cc8a1149314c0b7315b35d58359a8d1e5547707357f096491019d32d01ff182f547c3ff70fa38612597319407514c095ee54243e0e3f5be37a3de609953'
